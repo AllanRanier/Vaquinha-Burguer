3ce2f1a10273ed207dbc6f1b4c418df0235e5ee8
AllanRanier <allanranier18@gmail.com>
2022-02-01 15:43:56 -0300

    tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/core/bindings/application_binding.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha_burger_mobile/app/routes/auth_routes.dart';
 import 'package:vakinha_burger_mobile/app/routes/splash_routers.dart';
@@ -17,6 +18,7 @@ class VakinhaBurgerMainApp extends StatelessWidget {
     return  GetMaterialApp(
       title: 'Vakinha Burguer',
       theme: VakinhaUI.theme,
+      initialBinding: ApplicationBinding(),
       getPages: [
         ...SplashRouters.routers,
         ...AuthRouters.routers,
